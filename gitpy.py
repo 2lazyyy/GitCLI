b99add682ff7ed6949685eb32c30b04fb5c2b3ef
0x2lazy <andreigherman230808@gmail.com>
2026-01-28 22:59:13 +0200

    added flags and repo endpoint, removed caching

--- gitpy.py
+++ gitpy.py
@@ -1,28 +1,10 @@
 import requests
 import json
-import keyring
+import argparse
 
 base = 'https://api.github.com/'
-SERVICE = 'GitCLI'
-KEY = 'username'
-
-def login():
-    username = keyring.get_password(SERVICE, KEY)
-
-    if username:
-
-        return username
-
-    username = input("Enter username: ").strip()
-    keyring.set_password(SERVICE, KEY, username)
-
-    return username
-
-
-
 
 def get_events(username):
-
     url = f"{base}/users/{username}/events"
 
     response = requests.get(url)
@@ -32,12 +14,37 @@ def get_events(username):
         print(event["type"], "->", event["repo"]["name"], "AT THE TIME: ", event["created_at"])
 
 
+def get_repos(username):
+    url = f"{base}/users/{username}/repos"
+    
+    response = requests.get(url)
+    events = response.json()
+
+    for event in events:
+        print(event["name"], "\n\nDESCRIPTION\n", event["description"])
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--activity", action="store_true")
+    parser.add_argument("--username")
+    parser.add_argument("--repos", action="store_true")
+    args = parser.parse_args()
+
+    if args.activity and not args.username:
+        parser.error("--username is required when using --activity")
+
+    return args
+
+
 
 def main():
-    username = login()
-    get_events(username)
-
-
+    args = parse_args()
+    username = args.username
+    if args.activity:
+        get_events(username)
+    if args.repos:
+        get_repos(args.username)
 
 if __name__ == "__main__":
     main()
